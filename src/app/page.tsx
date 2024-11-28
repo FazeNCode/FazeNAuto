f92eca3c557ab2999bd82edf5274c22c2564c5e6
FazeNCode <FazeNCode@gmail.com>
2024-11-28 01:19:52 -0500

    replaced <a> with <Link> for naviagation, as it was causing conflict when deploying project to AWS Amplify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Image from "next/image";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Navbar from '@/components/Navbar'; 
-import Footer from '@/components/Footer'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
